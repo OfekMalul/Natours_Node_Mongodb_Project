33f5bf96ab2e8bb2bcc41b0455b46aac1e86ca65
Ofek Malul <ofekmalulmail@gmail.com>
2023-04-04 22:36:13 -0400

    adding authriziation

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -13,10 +13,15 @@ const {
 } = require('./../controllers/authController');
 const router = expresss.Router({ mergeParams: true }); // allows the params to go through, for ex' the tour id from the tours router.
 
+// all paths are authenticated below this middleware
+router.use(authenticatesUser);
+
 router.route('/').get(getAllReviews);
+router.route('/').post(restrictTo('user'), setTourUserIds, createReview);
 router
-  .route('/')
-  .post(authenticatesUser, restrictTo('user'), setTourUserIds, createReview);
-router.route('/:id').get(getReview).patch(updateReview).delete(deleteReview);
+  .route('/:id')
+  .get(getReview)
+  .patch(restrictTo('user', 'admin'), updateReview)
+  .delete(restrictTo('user', 'admin'), deleteReview);
 
 module.exports = router;
