0dfcd5f3a61f8415b76e06958d6c3c094c0abff2
Ofek Malul <ofekmalulmail@gmail.com>
2023-04-08 14:46:59 -0400

    Adding pug templates

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,6 +15,8 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 //1) global middlewares
 //Set Security HTTP headers
 app.use(helmet());
@@ -35,7 +38,7 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 // serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Data sanatization against noSQL query injections
 app.use(mongoSanitize());
@@ -64,6 +67,12 @@ app.use((req, res, next) => {
 });
 
 // //3) Routes
+//render the views
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+('');
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
