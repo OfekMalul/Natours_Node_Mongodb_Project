84844c95ce825658a2ecd306550f1872d2058e1d
Ofek Malul <ofekmalulmail@gmail.com>
2023-04-25 14:52:51 -0400

    adding functionallity to allow user upload new images from the UI

--- public/js/login.js
+++ public/js/login.js
@@ -80,6 +80,7 @@ const updateSettings = async (data, type) => {
     });
     if (res.data.status === 'success') {
       showAlert('success', `Account ${type} successfully updated!`);
+      window.setTimeout(location.reload(true), 500);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
@@ -89,9 +90,11 @@ const updateSettings = async (data, type) => {
 if (formUserData) {
   formUserData.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings({ name, email }, 'data');
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    updateSettings(form, 'data');
   });
 }
 
